b4e6938ef3f7a6d5c81e456e3b137c407b04aaf3
gabipique98 <gabriel.pique@hotmail.com>
2017-07-14 10:37:00 -0300

    Update Código.pas

--- Código.pas
+++ Código.pas
@@ -1,4 +1,4 @@
-
+(*Comision 1°02 - Berruti Octavio, Navarini Alejandro, Piqué Gabriel*) 
 Aca modifica todo
 program TpN2AyED;
 uses crt;
@@ -12,7 +12,7 @@ procedure CodigodeBarras;
 writeln('Menu De Opciones');
 writeln('1-Juegos');
 writeln('2-Calculos');
-writeln('3-Codigo de Barras/QR')
+writeln('3-Codigo de Barras/QR');
 writeln('4-Fin');
 readln(OP);
 repeat
