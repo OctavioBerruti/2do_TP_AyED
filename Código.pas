87fc035416522e756dd0f1cae45f37f9096b23fc
OctavioBerruti <octavioberruti8@gmail.com>
2017-07-15 19:52:40 -0300

    Lo hice andar putos

--- Código.pas
+++ Código.pas
@@ -2,39 +2,41 @@
 program TpN2AyED(input,output);
 uses crt;
 var OP:integer;
+    c:char;
+
+
 procedure Juegos;
-var arreglo:array[1..2,1..11] of char;
-    caracter: char;
-    i: integer;
- begin
-  i:=1;
-
-  writeln('Ingrese una palabra de maximo 10  caracteres y al finalizar ingrese "/"');
-  while (caracter<>'/') and (i<>11) do
-   begin 
-   read(caracter);
-   arreglo[1,i]:=caracter;
-   caracter:='';
-   i:=i+1;
-   
-   
-  end 
-
- end;
-procedure Calculos;
-procedure CodigodeBarras;
+var arreglos: array[1..2,1..11] of char;
+    caracteres: char;
+    i:integer;
 begin
-writeln('Menu De Opciones');
-writeln('1-Juegos');
-writeln('2-Calculos');
-writeln('3-Codigo de Barras/QR');
-writeln('4-Fin');
-while OP<>4 do
-readln(OP);
+    i:=1;
+  arreglos[1,i]:='a';
+  writeln('Ingrese una palabra de maximo 10  caracteres y al finalizar ingrese "/"');
+ while (caracteres<>'/') and (i<>11) do
+   begin 
+   readln(caracteres);
+   arreglos[1,i]:=caracteres;
+   i:=i+1;
+  end
 
-case OP of 
-1:Juegos;
-2:Calculos;
-3:CodigodeBarras;
-readkey;
+end;
+
+
+
+
+begin
+     OP:=0;
+     writeln('Menu De Opciones');
+     writeln('1-Juegos');
+     writeln('2-Calculos');
+     writeln('3-Codigo de Barras/QR');
+     writeln('4-Fin');
+     readln(OP);
+     case OP of 1:Juegos;
+                end;
+
+
+
+     c:=Readkey;
 end.
