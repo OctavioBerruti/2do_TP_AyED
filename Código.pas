e245eb2da0b3a10e117147f96b87f78139b86e5f
OctavioBerruti <octavioberruti8@gmail.com>
2017-07-15 19:47:49 +0200

    Empieza juego

--- Código.pas
+++ Código.pas
@@ -1,10 +1,18 @@
 (*Comision 1°02 - Berruti Octavio, Navarini Alejandro, Piqué Gabriel*) 
-program TpN2AyED;
+program TpN2AyED(input,output);
 uses crt;
 var OP:integer;
-function Juegos;
-function Calculos;
-function CodigodeBarras;
+function Juegos();
+ begin
+  writeln('Ingrese una palabra de maximo 10  caracteres y al finalizar ingrese "/"');
+  While caracter='/' or i=10 do
+   begin 
+   readln();
+   end 
+
+ end;
+function Calculos();
+function CodigodeBarras();
 begin
 writeln('Menu De Opciones');
 writeln('1-Juegos');
@@ -14,9 +22,9 @@ writeln('4-Fin');
 readln(OP);
 while OP<>4 do
 case OP of 
-1:Juegos;
-2:Calculos;
-3:CodigodeBarras;
+1:Juegos();
+2:Calculos();
+3:CodigodeBarras();
 readln(OP);
 readkey
 end.
